3fb25c698f38f63c4e7f563b8e61aedf6838640e
realcryptogates <dksd93@gmail.com>
2021-07-12 03:03:57 +0900

    added data models

--- src/entities/Log.ts
+++ src/entities/Log.ts
@@ -1,3 +1,11 @@
-class Log{
-    constructor(logid:string, asset_tag:string, date:string,techician:string, details: string){}
+/* eslint-disable */
+export default class Log{
+  constructor(
+    public id:string,
+    public asset_tag: string, 
+    public date:string, 
+    public tech: string, 
+    public description: string,
+    public problemType: string,
+    ){}
 }
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export interface IUser {
     id: number;
     name: string;
