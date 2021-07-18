26edd4f333d168cca3d1b29403cae0f31e83c154
David Quezada <dvd.quezada@gmail.com>
2021-07-18 15:24:51 -0700

    user model

--- db/entities/User.ts
+++ db/entities/User.ts
@@ -1,36 +1,7 @@
-/* eslint-disable */
-export interface IUser {
-    id: number;
-    name: string;
-    email: string;
-}
-export class MUser {
+export default class User {
     constructor(
-      public ID: string,
-      public Password: string,
-      public Role:role,
+      public username: string,
+      public password: string,
+      public role = 'Technician' || 'Admin',
     ) {}
-}
-class User implements IUser {
-
-    public id: number;
-    public name: string;
-    public email: string;
-    constructor(nameOrUser: string | IUser, password: string, email?: string, id?: number, role?: string
-        ) {
-        if (typeof nameOrUser === 'string') {
-            this.name = nameOrUser;
-            this.email = email || '';
-            this.id = id || -1;
-        } else {
-            this.name = nameOrUser.name;
-            this.email = nameOrUser.email;
-            this.id = nameOrUser.id;
-        }
-    }
-}
-
-// david's edit #2
-
-export type role = 'Admin' | 'Technician'
-export default User;
+  }
